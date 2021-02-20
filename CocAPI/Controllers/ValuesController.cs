da0aa0329e12ef3ec97a13e47b62d9a7f963d8b1
hayrikeskin <64478301+hayrikeskin@users.noreply.github.com>
2021-02-20 13:17:04 +0100

    initial commit

--- CocAPI/Controllers/ValuesController.cs
+++ CocAPI/Controllers/ValuesController.cs
@@ -1,7 +1,9 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CocAPI.Controllers
 {
@@ -10,15 +12,37 @@ namespace CocAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values/%238L000CQ8
+        private readonly IConfiguration configuration;
+        public ValuesController(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        // GET api/values/
+        [HttpGet()]
+        public ActionResult<string> Get()
+        {
+            return "Add clantag in url to retrieve clan info. Clantag must contain '%23' in place of #-symbol.";
+        }
+
+        // GET api/values/%23XX000XX0
         [HttpGet("{tag}")]
         public async Task<ActionResult<string>> GetAsync(string tag)
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA1OGM3MjU3LWJjZWMtNDAxNi1hNjIxLThhOWQyNDViNTBjOSIsImlhdCI6MTYxMzIwMDkzNywic3ViIjoiZGV2ZWxvcGVyL2QxNGExY2IxLWZlYmEtMDM5OC03NWU2LWZkNGVjMjcwMzhkMSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjgyLjE3My4zOS4yNDgiXSwidHlwZSI6ImNsaWVudCJ9XX0.CUlevIlUQEPWtpZoZbT3lI5oqk9uxMI4tVGW5XyCyIN2yDw2Y4YWE0AgEg3hyuy1-4CEJEKWXivMhY3DNlUx-Q");
+            HttpClient client = new HttpClient();client.DefaultRequestHeaders.Add("Authorization", "Bearer " + configuration.GetValue<string>("CoCBearer"));
             HttpResponseMessage response = await client.GetAsync("https://api.clashofclans.com/v1/clans/" + tag);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var error = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("{ \"memberList\" : [] }", System.Text.Encoding.UTF8, "application/json")
+                };
+                return await error.Content.ReadAsStringAsync();
+            }
         }
     }
 }
